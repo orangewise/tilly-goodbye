f894f1ee2d9fa236c6dca659a1f9002e9da914bd
Ronald Luitwieler <rluitwieler@anwb.nl>
2025-01-11 10:59:49 +0100

    add url

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(
     name="tilly-goodbye",
     version="0.0.1",
+    url="https://github.com/tilly-pub/tilly-goodbye",
     py_modules=["tilly_goodbye"],
     install_requires=[
         "tilly",
